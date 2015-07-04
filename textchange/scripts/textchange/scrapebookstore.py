640fe4e14013500975f13e83ccf33ce9542d033e
ProgrammingJoe <joeczepil@gmail.com>
2015-07-03 19:22:41 -0700

    Complete textbook strip

--- textchange/scripts/textchange/scrapebookstore.py
+++ textchange/scripts/textchange/scrapebookstore.py
@@ -5,8 +5,45 @@ soup = BeautifulSoup(open("bookstore.html"))
 
 books = soup.findAll("div", { "class" : "textbook-item"})
 
-for x in books:
-    for y in x.find("b"):
-        print("%s\n"% y)
-    for z in x.select("[class~=author]"):
-        print("%s\n"% z.contents)
+def cleanstr(str):
+    str = str.replace('\n','')
+    str = str.replace(' ','')
+    return str
+
+def findbook(x):
+    tag = x.find("b")
+    print("%s"% tag.string)
+
+def findauthor(x):
+    tag = x.find("span", { "class" : "author"})
+    author = tag.string
+    author = cleanstr(author)
+    author = author.replace("Author:", "")
+    print("%s"% author)
+
+def findcourse(x):
+    parent1 = x.parent
+    parent2 = parent1.parent
+    parent3 = parent2.parent
+    info = parent3.find("div", { "class" : "row course-info"})
+    coursetag = info.find('span')
+    course = coursetag.string
+    course = cleanstr(course)
+    print("%s"% course)
+
+def findisbn(x):
+    secondrow = x.findAll("div", { "class" : "row"})[1]
+    link = secondrow.find('a')
+    isbn =  link.string
+    isbn = cleanstr(isbn)
+    print("%s\n"% isbn)
+
+def main():
+    for x in books:
+        findbook(x)
+        findauthor(x)
+        findcourse(x)
+        findisbn(x)
+
+
+main()
