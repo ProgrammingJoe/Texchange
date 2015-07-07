215ac5f5b1b03a54d5c622a3f43d78b438cbef7e
Programmingjoe <joeczepil@gmail.com>
2015-07-07 12:38:01 -0700

    Fixed user creation form

--- textchange/forms.py
+++ textchange/forms.py
@@ -8,11 +8,18 @@ from django import forms
 from .models import User, Posting
 
 class UserCreate(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ('username', 'password1', 'password2', 'email', 'phone')
+		widgets = {
+		'password1': forms.PasswordInput(),
+		'password2': forms.PasswordInput(),
+		}
 	username = forms.CharField()
 	password1 = forms.CharField()
 	password2 = forms.CharField()
 	email = forms.EmailField()
-	
+	phone = forms.CharField()
+
 class SearchForm(forms.Form):
 	keywords = forms.CharField()
-	
