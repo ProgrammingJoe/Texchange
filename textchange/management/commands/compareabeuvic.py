3b834b9a0139e61b7293cdb4fe520f3f198d5edc
Programmingjoe <joeczepil@gmail.com>
2016-01-26 20:27:05 -0800

    Scrape prices from abebooks and bookstore

--- textchange/management/commands/compareabeuvic.py
+++ textchange/management/commands/compareabeuvic.py
@@ -6,11 +6,37 @@ import operator
 
 
 class Command(BaseCommand):
-    def bookstoreprice():
-        print("one")
+    def bookstoreprice(self, soup):
+        new = "0"
+        used = "0"
+        prices = soup.find("div", {"class": "textbook-item"})
+        prices2 = prices.findAll("span", {"class": "right"})
+        for y in prices2:
+            if "Used" in y.string:
+                used = y.string
+            elif "New" in y.string:
+                new = y.string
+        new = new.replace(' ', '')
+        new = new[6:]
+        used = used.replace(' ', '')
+        used = used[7:]
+        return(new, used)
 
-    def abeprice():
-        print("two")
+    def abeprice(self, soup):
+        prices = soup.findAll("div", {"class": "cf result"})
+        for x in prices:
+            prices2 = x.find("div", {"class": "result-pricing col-xs-4"})
+            startprice = prices2.find("div", {"class": "item-price"})
+            shipprice = prices2.find("div", {"class": "shipping"})
+            startprice = startprice.find("span")
+            shipprice = shipprice.find("span")
+            startprice = startprice.string
+            shipprice = shipprice.string
+            print("@@@@@@@@@@@@@@@@@")
+            print(startprice)
+            print(shipprice)
+            # print(x)
+
 
     # Main function to call all the methods
     def handle(self, *args, **options):
@@ -18,18 +44,12 @@ class Command(BaseCommand):
         abebase_two = "&sortby=96&sts=t"
         uvicbase = "https://www.uvicbookstore.ca/text/book/"
 
-        comparison = {}
-
-        textbooks = Textbook.object.values('isbn').distinct
-        for book in textbooks:
-            abepage = urllib2.urlopen(abebase_one + book.isbn + abebase_two).read()
-            uvicpage = urllib2.urlopen(uvicbase + book.isbn)
-            abesoup = BeautifulSoup(abepage)
-            uvicsoup = BeautifulSoup(uvicpage)
-            price1 = self.bookstoreprice(uvicsoup)
-            price2 = self.abeprice(abesoup)
-            if price1 and price2:
-                comparison[book.isbn] = float(price1) - float(price2)
-
-        sorted_prices = sorted(comparison.items(), key=operator.itemgetter(1))
-        print(sorted_prices[:20])
+        # textbooks = Textbook.objects.all().distinct('isbn')
+        isbn = "9780321573513"
+        # for book in textbooks:
+        abepage = urllib2.urlopen(abebase_one + isbn + abebase_two).read()
+        uvicpage = urllib2.urlopen(uvicbase + isbn)
+        abesoup = BeautifulSoup(abepage)
+        uvicsoup = BeautifulSoup(uvicpage)
+        price1 = self.bookstoreprice(uvicsoup)
+        price2 = self.abeprice(abesoup)
