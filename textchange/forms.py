e75cad50136545814c2414785892089fdbd4248f
Programmingjoe <joeczepil@gmail.com>
2017-01-24 20:38:21 -0800

    styling index and results

--- textchange/forms.py
+++ textchange/forms.py
@@ -17,8 +17,8 @@ class PostCreate(forms.Form):
     CHOICES = (('New', 'New'), ('Like New', 'Like New'), ('Used', 'Used'), ('Usable', 'Usable'))
     price = forms.DecimalField(max_digits=5, decimal_places=2, widget=forms.TextInput(attrs={'class': 'add_posting_field'}), error_messages={'Invalid': 'Price must be of a max of 5 numbers', 'required': 'A price is required to post.'})
     condition = forms.ChoiceField(choices=CHOICES, widget=forms.Select(attrs={'class': 'add_posting_field'}))
-    image = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'class': 'add_posting_field'}))
-    comments = forms.CharField(max_length=50, widget=forms.Textarea(attrs={'class': 'add_posting_field'}), required=False)
+    image = forms.ImageField(required=False, widget=forms.ClearableFileInput(attrs={'class': 'add_posting_image_field'}))
+    comments = forms.CharField(max_length=50, widget=forms.Textarea(attrs={'class': 'add_posting_comments_field'}), required=False)
 
 
 # The contact form is used to save information in the feedback model
